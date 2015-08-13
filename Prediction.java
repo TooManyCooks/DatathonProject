de7a4873b8d634fbe8074f78f9fa21d5aca736d7
Jake McDermott <JAKE.MCDERMOTT.2014@nuim.ie>
2015-08-14 00:07:19 +0100

    Create Prediction.java

---
+++ Prediction.java
@@ -0,0 +1,32 @@
+package Utility;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.opencsv.*;
+
+public class Prediction extends mainClass {
+	static int userID;
+	static int movieID;
+	static double movieAverage;
+	static int[] genreArray;
+	static ArrayList<int[]> ratingsArray;
+	static int prediction;
+	static ArrayList<Integer> moviePreference;
+	
+	public void setValues(int user, int movie) {
+		userID 			= user;
+		movieID 		= movie;
+		movie mov 		= movies.get(movie-1);
+		movieAverage 	= mov.getAverage();
+		genreArray 		= mov.getGenre();
+		user userRef	= new user();
+		ratingsArray 	= userRef.getRatings();
+	}
+	public double getPrediction() {
+		return movieAverage;
+	}
+}
